27c3af31b5a4ffad2a18a3ee5e8ef7146e47683e
Kimchi13062003 <156589998+Kimchi13062003@users.noreply.github.com>
2025-05-08 17:08:21 +0700

    Add files via upload

---
+++ Reflection_JUnit_Calculator/src/main/java/MyCalculator_String/String_New.java
@@ -0,0 +1,39 @@
+package MyCalculator_String;
+
+public class String_New {
+	
+	//Ghép chuỗi
+	private String concat(String a, String b) {
+        return a + b;
+    }
+	
+	// Đếm độ dài chuỗi
+    public int length(String str) {
+        return str.length();
+    }
+
+    // Kiểm tra chuỗi có rỗng không
+    public boolean isEmpty(String str) {
+        return str.isEmpty();
+    }
+
+    // Chuyển chuỗi sang chữ in hoa
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    // Chuyển chuỗi sang chữ thường
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+    // So sánh hai chuỗi
+    public boolean equalsString(String a, String b) {
+        return a.equals(b);
+    }
+
+    // Cắt chuỗi từ chỉ số start đến end
+    public String substring(String str, int start, int end) {
+        return str.substring(start, end);
+    }
+}
